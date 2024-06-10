928442c27b25f4dfcb40a0ba5da543b2e3389e4c
Andrew Costa <andrewcosta.contato@gmail.com>
2024-06-10 20:08:00 -0300

    feat: server/ creater server

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+require('dotenv').config();
+import { wishlistRoutes } from "./routes";
+
+const cors = require('cors');
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(wishlistRoutes);
+
+app.listen(PORT, () => {
+  console.log(`O servidor está rodando em http://localhost:${PORT}`);
+});
