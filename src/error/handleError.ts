73812caa555ca8473fc8ba84994d6578576fb118
Andrew Costa <andrewcosta.contato@gmail.com>
2024-07-23 18:28:35 -0300

    feat: handleError/ create validation error handling

--- src/error/handleError.ts
+++ src/error/handleError.ts
@@ -1,8 +1,14 @@
 import { Response } from 'express';
 import HttpStatusError from './HttpStatusError';
+import ValidationError from './ValidationError';
 
 export default function handleError(response: Response, error: Error | HttpStatusError, statusCode: number) {
-  if (error instanceof HttpStatusError) {
+  if (error instanceof ValidationError) {
+    response.status(error.statusCode).json({
+      message: error.message,
+      details: error.details
+    });
+  } else if (error instanceof HttpStatusError) {
     response.status(error.statusCode).json({ message: error.message })
   } else if (error instanceof Error) {
     response.status(statusCode).json({ message: error.message });
