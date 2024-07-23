91b4d988ce2d35aa435a42c03fddcc9ed3ad8c0c
Andrew Costa <andrewcosta.contato@gmail.com>
2024-07-23 15:47:24 -0300

    feat: error/ create error handling

---
+++ src/error/handleError.ts
@@ -0,0 +1,12 @@
+import { Response } from 'express';
+import HttpStatusError from './HttpStatusError';
+
+export default function handleError(response: Response, error: Error | HttpStatusError, statusCode: number) {
+  if (error instanceof HttpStatusError) {
+    response.status(error.statusCode).json({ message: error.message })
+  } else if (error instanceof Error) {
+    response.status(statusCode).json({ message: error.message });
+  } else {
+    response.status(500).json({ message: "Interval server error" });
+  }
+};
