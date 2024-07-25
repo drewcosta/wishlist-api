0bcac00180e599b24192a6ebd13e153e89b5a804
Andrew Costa <andrewcosta.contato@gmail.com>
2024-07-25 19:59:30 -0300

    feat: services/createWishlistItem/ extract response formatting and changing parameter name

--- src/services/wishlist/create-wishlistitem.ts
+++ src/services/wishlist/create-wishlistitem.ts
@@ -1,25 +1,20 @@
 import { ZodError } from "zod";
 import { HttpStatusError, ValidationError } from "../../error";
 import { IWishlistItem } from "../../interfaces";
-import { IServiceResponse } from "../../interfaces/IServiceResponse";
 import { wishlistRepository } from "../../repositories";
 import { wishlistItemSchema, WishlistItemInput } from "../../schemas/wishlist/wishlistitemSchema";
 
-export async function createWishlistItem(item: WishlistItemInput): Promise<IServiceResponse<IWishlistItem>> {
+export async function createWishlistItem(inputItem: WishlistItemInput): Promise<IWishlistItem> {
   try {
-    wishlistItemSchema.parse(item);
-    const newItem = await wishlistRepository.createItem(item);
+    wishlistItemSchema.parse(inputItem);
+    const newItem = await wishlistRepository.createItem(inputItem);
 
-    return {
-      success: true,
-      data: newItem,
-      message: "Item criado com sucesso!",
-    };
+    return newItem;
   } catch (error) {
     if (error instanceof ZodError) {
       const flattenedErrors = error.flatten();
       throw new ValidationError("Dados inválidos para cadastro", 400, flattenedErrors);
     }
-    throw new HttpStatusError("Não foi possível criar um novo item", 400);
+    throw error;
   }
 }
