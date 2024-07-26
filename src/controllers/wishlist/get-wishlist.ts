c568ef011c39ebd76363944bca81c6c2914bc264
Andrew Costa <andrewcosta.contato@gmail.com>
2024-07-25 21:37:15 -0300

    feat: getWishlist/ add response formatting

--- src/controllers/wishlist/get-wishlist.ts
+++ src/controllers/wishlist/get-wishlist.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import { wishlistService } from '../../services';
 import { handleError } from '../../error';
+import { IControllerResponse } from '../../interfaces/IControllerResponse';
 
 export async function getWishlist(req: express.Request, res: express.Response): Promise<void> {
   try {
     const wishlist = await wishlistService.getWishlist();
 
-    res.status(200).json(wishlist);
+    const response: IControllerResponse<typeof wishlist> = {
+      success: true,
+      data: wishlist,
+      message: "Todos os itens capturados com sucesso!"
+    }
+
+    res.status(200).json(response);
   } catch (error: any) {
     handleError(res, error, 500);
   }
