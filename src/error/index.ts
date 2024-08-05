de18657d9e1a960d7c4a6c8ed6983ffa70258378
Andrew Costa <andrewcosta.contato@gmail.com>
2024-08-05 15:26:41 -0300

    remove validationError class

--- src/error/index.ts
+++ src/error/index.ts
@@ -1,9 +1,7 @@
 import handleError from "./handleError";
 import HttpStatusError from "./HttpStatusError";
-import ValidationError from "./ValidationError";
 
 export {
   handleError,
   HttpStatusError,
-  ValidationError
 }
