d2158dfa1a18f15742bd82132a4481342f67e1b2
Andrew Costa <andrewcosta.contato@gmail.com>
2024-08-06 10:07:47 -0300

    feat: wishlist routes/ add middleware to the route

--- src/routes/wishlist.routes.ts
+++ src/routes/wishlist.routes.ts
@@ -1,12 +1,35 @@
 import express from 'express';
 import { wishlistController } from '../controllers';
+import { wishlistMiddlewares } from '../middlewares';
 
 const router = express.Router();
 
-router.get('/wishlist', wishlistController.getWishlist);
-router.get('/wishlist/:id', wishlistController.getWishlistItemById);
-router.post('/wishlist', wishlistController.createWishlistItem);
-router.put('/wishlist/:id', wishlistController.updateWishlistItemById);
-router.delete('/wishlist/:id', wishlistController.deleteWishlistItemById);
+router.get(
+  '/wishlist',
+  wishlistController.getWishlist
+);
+
+router.get(
+  '/wishlist/:id',
+  wishlistController.getWishlistItemById
+);
+
+router.post(
+  '/wishlist',
+  wishlistMiddlewares.verifyItemBody,
+  wishlistController.createWishlistItem
+);
+
+router.put(
+  '/wishlist/:id',
+  wishlistMiddlewares.verifyItemBody,
+  wishlistController.updateWishlistItemById
+);
+
+router.delete(
+  '/wishlist/:id',
+  wishlistController.deleteWishlistItemById
+
+);
 
 export default router;
