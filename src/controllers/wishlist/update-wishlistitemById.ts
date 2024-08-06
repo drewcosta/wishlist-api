1ff9ecfce77a04fd7bb99753fe69e1c037a53197
Andrew Costa <andrewcosta.contato@gmail.com>
2024-08-06 10:07:11 -0300

    refactor: updateWishlistItem/ destructuring req body

--- src/controllers/wishlist/update-wishlistitemById.ts
+++ src/controllers/wishlist/update-wishlistitemById.ts
@@ -2,13 +2,19 @@ import express from 'express';
 import { wishlistService } from '../../services';
 import { handleError } from '../../error';
 import { IControllerResponse } from '../../interfaces/IControllerResponse';
+import { IWishlistItem } from '../../interfaces';
 
 export async function updateWishlistItemById(req: express.Request, res: express.Response) {
   try {
-    const inputItem = req.body;
-    const itemId = req.params.id;
+    const { name, price } = req.body as IWishlistItem;
+    const { id: itemId } = req.params;
 
-    const updatedItem = await wishlistService.updateWishlistItemById(itemId, inputItem);
+    const params = {
+      name,
+      price
+    }
+
+    const updatedItem = await wishlistService.updateWishlistItemById(itemId, params);
 
     const response: IControllerResponse<typeof updatedItem> = {
       success: true,
