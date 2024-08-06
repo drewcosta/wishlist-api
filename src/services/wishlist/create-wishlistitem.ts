b9733930c815adfda13f7e75022b287e7663e649
Andrew Costa <andrewcosta.contato@gmail.com>
2024-08-06 10:11:04 -0300

    refactor: createWishlistItem service/ extracting trycatch

--- src/services/wishlist/create-wishlistitem.ts
+++ src/services/wishlist/create-wishlistitem.ts
@@ -1,20 +1,7 @@
-import { ZodError } from "zod";
-import { HttpStatusError, ValidationError } from "../../error";
 import { IWishlistItem } from "../../interfaces";
 import { wishlistRepository } from "../../repositories";
-import { wishlistItemSchema, WishlistItemInput } from "../../schemas/wishlist/wishlistitemSchema";
 
-export async function createWishlistItem(inputItem: WishlistItemInput): Promise<IWishlistItem> {
-  try {
-    wishlistItemSchema.parse(inputItem);
-    const newItem = await wishlistRepository.createItem(inputItem);
-
-    return newItem;
-  } catch (error) {
-    if (error instanceof ZodError) {
-      const flattenedErrors = error.flatten();
-      throw new ValidationError("Dados inválidos para cadastro", 400, flattenedErrors);
-    }
-    throw error;
-  }
+export async function createWishlistItem(inputItem: IWishlistItem): Promise<IWishlistItem> {
+  const newItem = await wishlistRepository.createItem(inputItem);
+  return newItem;
 }
